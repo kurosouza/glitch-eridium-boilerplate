9487011c4ece9af9ea0b615df90019da4d2c6750
Kuro Souza <kurosouza@gmail.com>
2018-12-18 21:48:33 +0100

    Initial commit.

---
+++ app/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<h1>hello from an es6-enabled glitch app.</h1>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./app/index.js"
+  },
+  output: {
+    filename: 'public/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, 
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react', 'stage-3']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({filename: "[name].css", allChunks: true})
+  ]
+}
