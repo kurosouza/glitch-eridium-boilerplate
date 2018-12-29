1a0d96d47275d41e6e07be350af738a7665541d9
Kuro Souza <kurosouza@gmail.com>
2018-12-29 11:14:48 +0100

    Minor updates.

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -6,8 +6,8 @@ class Home extends Component {
     render() {
         return (
             <Box full color='brand' direction='column' align='center'>
-                <Box margin='small'><Heading level={4}>Eridium React</Heading></Box>
-                <Box margin='medium'><TextInput /></Box>
+                <Box margin='xsmall'><Heading level={4}>Eridium React</Heading></Box>
+                <Box margin='small'><TextInput size='medium'/></Box>
                 <Box margin='small'>
                     <Button label='check' color='accent-1'></Button>
                 </Box>
